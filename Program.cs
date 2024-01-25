dfbb91a57fedee05f5a46c97ced3de99e5fa519e
Connor Hopkins <conhop22@yahoo.com>
2024-01-25 15:05:30 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿
+/***************************
+ * Playing the game section.
+ ***************************/
+
+void advancePath()
+{
+
+}
+
+int getPrompt()
+{
+    return 0;
+}
+
+void displayPrompt()
+{
+
+}
+
+void displayAnswerOptions()
+{
+
+}
+
+int getAnswer()
+{
+    return 0;
+}
+
+// TODO: Stretch Goal.
+void saveGame()
+{
+
+}
+
+// Check if the game is over.
+bool isOver()
+{
+    return false;
+}
+
+/********************
+ * Game over section.
+ ********************/
+
+// TODO: Stretch Goal.
+int loadSave()
+{
+    return 0;
+}
+
+void displayPathTaken()
+{
+
+}
+
+// Driver of the program.
+static void main()
+{
+
+}
+
+/************************
+ * Start the program.
+ ************************/
+main();
