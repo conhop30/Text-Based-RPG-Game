82e8091f9de75ab15d13f3276eef9b35e71217f1
Connor Hopkins <conhop22@yahoo.com>
2024-01-26 12:15:30 -0700

    Added file reading and writing to the terminal through function calls.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
 ﻿
-/***************************
- * Playing the game section.
- ***************************/
+using System.IO; // For reading files.
+
+/***********************
+ * File reading section.
+ ***********************/
+
+static string[] readFile(string path)
+{
+    // Try-Catch verification.
+    try
+    {
+        using (StreamReader reader = new StreamReader(path))
+        {
+            return File.ReadAllLines(path);
+        }
+    }
+    catch (Exception e)
+    {
+        throw new IOException($"Error reading file: {path}", e);
+    }
+}
+
+/******************
+* Gameplay section.
+*******************/
 
 void advancePath()
 {
@@ -13,9 +35,9 @@ int getPrompt()
     return 0;
 }
 
-void displayPrompt()
+static void displayPrompt(int nextPrompt)
 {
-
+    Console.WriteLine("Inside displayPrompt.");
 }
 
 void displayAnswerOptions()
@@ -34,15 +56,14 @@ void saveGame()
 
 }
 
-// Check if the game is over.
+/*********************
+* "Game over" section.
+**********************/
+
 bool isOver()
 {
     return false;
 }
-
-/********************
- * Game over section.
- ********************/
 
 // TODO: Stretch Goal.
 int loadSave()
@@ -56,12 +77,41 @@ void displayPathTaken()
 }
 
 // Driver of the program.
-static void main()
+static void Main(string[] args)
 {
+    // Declare starting variables.
+    int nextPrompt = 0;
 
+    // Load in necessary files.
+    string[] promptFile = readFile("C:\\Users\\conho\\source\\repos\\Text-Based RPG Game\\promptFile.txt");
+    string[] answerFile = readFile("C:\\Users\\conho\\source\\repos\\Text-Based RPG Game\\answerFile.txt");
+
+    // TODO: Data verification. Remove this.
+    foreach (var  prompt in promptFile)
+    {
+        Console.WriteLine(prompt);
+    }
+
+    foreach (var answer in answerFile)
+    {
+        Console.WriteLine(answer);
+    }
+
+    // Start the game.
+    Console.WriteLine("||===================================||");
+    Console.WriteLine("|| Welcome to Text-Based RPG Game... ||");
+    Console.WriteLine("||===================================||");
+    Console.WriteLine("");
+    Console.WriteLine("This is the starting prompt for the game.");
+    Console.WriteLine("This is the starting prompt for the game again.");
+    Console.WriteLine("This is the starting prompt for the game a third time.");
+    Console.WriteLine("This is the starting prompt for the game for the final time.");
+
+    // (will-be) Game loop.
+    displayPrompt(nextPrompt);
 }
 
 /************************
- * Start the program.
- ************************/
-main();
+* Start the program.
+************************/
+Main(args);
--- README.md
+++ README.md
@@ -6,19 +6,22 @@
 ### Programming Language
 	C#
 
-### Time Spent
+### Time Log
+	30 minutes to create hand drawn structure chart.
 	15 minutes to create stubs.
-
+	30 minutes to make "Hello, World!" display in a function call.
+	37 minutes to read files.
 	--------------------------
-	Total time: 15 mintues
+	Total time: 1 Hour 52 mintues
 
 ### Planning Documents
+<!--
 ![Structure Chart]()
 
 [Flowcharts]()
 
 [Pseudocode]()
-
+-->
 ### Enviroment
 	Visual Studio
 
